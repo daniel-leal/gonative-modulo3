d00aa9f58e637dc8ee6be3816e9f0d917b0c6b59
Daniel Leal <danielleal94@gmail.com>
2019-07-17 16:48:25 -0300

    init repo

---
+++ src/pages/Login/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import api from '~/services/api';
+import * as LoginActions from '~/store/actions/login';
+
+import {
+  Container, Input, Button, ButtonText, Error,
+} from './styles';
+
+class Login extends Component {
+  state = { username: '' };
+
+  handleSubmit = async () => {
+    const { username } = this.state;
+    const { loginSuccess, loginFailure, navigation } = this.props;
+
+    try {
+      await api.get(`/users/${username}`);
+
+      loginSuccess(username);
+
+      navigation.navigate('Repositories');
+    } catch (err) {
+      loginFailure();
+    }
+  };
+
+  render() {
+    const { username } = this.state;
+    const { error } = this.props;
+
+    return (
+      <Container>
+        {error && <Error>Usuário inexistente</Error>}
+
+        <Input
+          value={username}
+          onChangeText={text => this.setState({ username: text })}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite seu usuário"
+        />
+        <Button onPress={this.handleSubmit}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  error: state.login.error,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(LoginActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Login);
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+
+import reducer from './reducers';
+
+const composer = __DEV__
+  ? compose(
+    applyMiddleware(...[]),
+    console.tron.createEnhancer(),
+  )
+  : applyMiddleware(...[]);
+
+const store = createStore(reducer, composer);
+
+console.tron.log(store.getState());
+
+export default store;
